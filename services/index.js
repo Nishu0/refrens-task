ffb9a7b74738956c0f8865536f94a3d03bd7baa5
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-12-17 00:54:10 +0530

    :rocket: Services and Images

---
+++ services/index.js
@@ -0,0 +1,25 @@
+const controller = new AbortController()
+
+async function getDatos(page) {
+    try {
+        const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+        const dados = await response.json()
+        return dados
+    } catch (error) {
+        if(error.response?.status === 404 || error.message === "Network Error") {
+            controller.abort();
+        } else {
+            console.error("Error", error);
+            throw error;
+        }
+        
+    }
+}
+
+async function getData(id) {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+    const datos = await response.json()
+    return datos
+}
+
+export { getDatos, getData }
